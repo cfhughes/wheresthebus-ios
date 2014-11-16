3fb4f29f7441132d271160a2c67fdacd664d9aaa
Chris Hughes <cfhughes@users-MacBook-Pro.local>
2014-11-15 21:11:29 -0700

    Initial Commit

---
+++ ABQWTBTests/ABQWTBTests.swift
@@ -0,0 +1,36 @@
+//
+//  ABQWTBTests.swift
+//  ABQWTBTests
+//
+//  Created by Chris Hughes on 11/15/14.
+//  Copyright (c) 2014 Chris Hughes. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ABQWTBTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
